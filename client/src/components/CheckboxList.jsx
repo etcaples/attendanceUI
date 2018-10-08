8f3de73150dd30905f51a5ba4892af08215f7b83
Emily Caples <etcaples@gmail.com>
2018-10-08 11:06:58 -0700

    added initial styling with styled components

--- client/src/components/CheckboxList.jsx
+++ client/src/components/CheckboxList.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Checkbox from './Checkbox';
+
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+  height: 30px;
+  font-size: 15px;
+`;
 
 const CheckboxList = (props) => {
   const { cohorts } = props;
@@ -9,15 +19,16 @@ const CheckboxList = (props) => {
       {
         cohorts.map(cohort => <Checkbox cohort={cohort} />)
       }
-      <button type="button">
-        check attendance (no LOG)
-      </button>
+      <br />
+      <Button type="button">
+        run attendance
+      </Button>
     </div>
   );
 };
 
 CheckboxList.propTypes = {
-  cohorts: PropTypes.arrayOf(PropTypes.string).isRequired,
+  cohorts: PropTypes.arrayOf(Object).isRequired,
 };
 
 export default CheckboxList;
